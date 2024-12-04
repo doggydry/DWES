cda7abbd9eb35cdacc5a5551f29472832879b3d1
doggydry <nestor.sernavaquero@iesmiguelherrero.com>
2024-12-04 18:48:54 +0100

    eje03

--- PHP/UNIDAD_05/Hoja05_Sesiones_03/public/visitas.php
+++ PHP/UNIDAD_05/Hoja05_Sesiones_03/public/visitas.php
@@ -4,6 +4,15 @@ session_start();
 $fechaActual = date('d-m-Y H:i:s');
 $instanteAnterior = isset($_SESSION['instante']) ? $_SESSION['instante'] : 'No se tiene registro de la última visita';
 $_SESSION['instante'] = $fechaActual;
+//? Definimos el contador
+if (isset($_SESSION['contador'])){
+     $_SESSION['contador']++;
+     $contador = $_SESSION['contador'];
+} else {
+    $_SESSION['contador'] = 0;
+    $contador = 'Primera vez';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +28,7 @@ $_SESSION['instante'] = $fechaActual;
     <h1>Sesiones</h1>
     <hr>
 <!-- Si la sesion ya se ha registrado, que se vaya incrementando, sino que se inicialice en 1 -->
-    <h4>Número de veces accedidas a esta página: <?php echo isset($_SESSION['contador']) ? $_SESSION['contador']++ : 'Primera vez' ?></h4>
+    <h4>Número de veces accedidas a esta página: <?php echo $contador ?></h4>
     <hr>
     <h4>Instante en el que se accedió a esta página: <?php echo $instanteAnterior?></h4>
 
