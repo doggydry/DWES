2bb79a775a18438a723f151715d408391ba6cb19
nin <nestor.sernavaquero@iesmiguelherrero.com>
2025-01-15 19:39:26 +0100

    zoologico cambios

--- PHP/Zoologico/Laravel_Zoologico/resources/views/animales/index.blade.php
+++ PHP/Zoologico/Laravel_Zoologico/resources/views/animales/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.plantilla')
 @section('titulo', 'Zoologico')
 @section('contenido')
-<h1 class="text-3xl font-bold underline">Listado de animales</h1>
+    <h1 class="text-3xl font-bold underline">Listado de animales</h1>
+    @foreach ($animales as $animal)
+        <ul>
+            <li>
+                <a href="animales/{$animal}" class="hover:underline hover:text-blue-500"><strong>Especie: </strong>{{ $animal['especie'] }}</a>
+                <strong>Peso: </strong>{{ $animal['peso'] }}cm
+                <strong>Altura: </strong>{{ $animal['altura'] }}KG
+                <strong>Fecha de Nacimiento: </strong>{{ $animal['fechaNacimiento'] }}
+            </li>
+            <br>
+        </ul>
+    @endforeach
 @endsection
--- PHP/Zoologico/Laravel_Zoologico/resources/views/animales/show.blade.php
+++ PHP/Zoologico/Laravel_Zoologico/resources/views/animales/show.blade.php
@@ -2,4 +2,9 @@
 @section('titulo','Zoologico')
 @section('contenido')
 <h1 class="text-3xl font-bold underline">Informacion sobre los animales</h1>
+@foreach ($animales as $animal)
+<ul>
+    <li>Nombre: {{$animal['especie']}}</li>
+</ul>
+@endforeach
 @endsection
