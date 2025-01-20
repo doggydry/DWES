00461d4151d950a86ce7e107fb6dcedf88d4975e
doggydry <nestor.sernavaquero@iesmiguelherrero.com>
2025-01-20 12:52:26 +0100

    punto

--- PHP/Zoologico/Laravel_Zoologico/resources/views/animales/show.blade.php
+++ PHP/Zoologico/Laravel_Zoologico/resources/views/animales/show.blade.php
@@ -26,8 +26,5 @@
 <div class="mb-4 text-center">
     <button type="submit" class="w-full py-3 bg-blue-600 text-white text-lg font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">Modificar Animal</button>
 </div> --}}
-
-
 </div>
-
 @endsection
