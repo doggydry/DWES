0a574d9eb9a6b4560bba228d1784bfa5fb932233
doggydry <nestor.sernavaquero@iesmiguelherrero.com>
2025-01-24 12:07:15 +0100

    logo

--- PHP/Zoologico/Laravel_Zoologico/resources/views/layouts/partials/nav.blade.php
+++ PHP/Zoologico/Laravel_Zoologico/resources/views/layouts/partials/nav.blade.php
@@ -6,7 +6,7 @@
                 <img
                     src="{{ asset('assets/imagenes/logo.png') }}"
                     alt="logo animal"
-                    class="h-20 w-auto transition-transform duration-300 ease-in-out transform hover:scale-110 hover:rotate-6"
+                    class="h-20 w-auto rounded-md transition-opacity duration-300 ease-in-out hover:opacity-80"
                 >
             </a>
         </div>
@@ -15,21 +15,21 @@
         </div>
         <div id="menu" class="hidden flex-grow w-full justify-between items-center absolute top-40 left-0 bg-green-800 py-14 px-8 lg:flex lg:relative lg:top-0 lg:py-0 lg:px-0 sm:px-14">
             <div class="flex flex-col mb-8 lg:flex-row lg:mb-0 mx-auto">
-                <a href="{{ route('inicio') }}" class="text-xl font-semibold text-white bg-green-600 py-2 px-6 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
+                <a href="{{ route('inicio') }}" class="text-lg font-semibold text-white bg-green-600 py-1 px-4 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
                     Inicio
                 </a>
-                <a href="{{ route('animales.index') }}" class="text-xl font-semibold text-white bg-green-600 py-2 px-6 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
+                <a href="{{ route('animales.index') }}" class="text-lg font-semibold text-white bg-green-600 py-1 px-4 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
                     Listado de Animales
                 </a>
-                <a href="{{ route('animales.create') }}" class="text-xl font-semibold text-white bg-green-600 py-2 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
+                <a href="{{ route('animales.create') }}" class="text-lg font-semibold text-white bg-green-600 py-1 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
                     Nuevo Animal
                 </a>
             </div>
             <div class="flex flex-col text-center lg:flex-row">
-                <a href="#" class="text-xl font-semibold text-green-800 bg-white py-2 px-6 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-700 hover:text-white hover:shadow-xl">
+                <a href="#" class="text-lg font-semibold text-green-800 bg-white py-1 px-4 rounded-lg shadow-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out hover:bg-green-700 hover:text-white hover:shadow-xl">
                     Iniciar Sesión
                 </a>
-                <a href="#" class="text-xl font-semibold text-white bg-green-600 py-2 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
+                <a href="#" class="text-lg font-semibold text-white bg-green-600 py-1 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500 hover:shadow-xl">
                     Regístrate
                 </a>
             </div>
